42fbbbbff5d9b71af198f30bcae4332ec79b5c5f
Tuyoni <wangcodingrun@gmail.com>
2018-10-16 19:28:20 +0900

    study1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Counter from "Components/Counter";
+import EnjoyLife from "Components/EnjoyLife";
+
+
+// class Tds extends Component {
+//   render() {
+//     return (
+//       <>
+//         <td>1</td>
+//         <td>2</td>
+//       </>
+//     );
+//   }
+// }
+
+const Tds = ({name}) => {
+  return (
+    <>
+      <td>{name}</td>
+      <td>2</td>
+    </>
+  )
+}
+
+class Table extends Component {
+  render() {
+    return (
+      <table>
+        <tr>
+          <Tds name="LEE"/>
+        </tr>
+      </table>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <Counter name="전지현"/>
+        <EnjoyLife/>
+        <Table/>
+      </>
     );
   }
 }
