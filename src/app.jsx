2574b951f819be2e807709d8e9b4a4d8e20931ad
Tuyoni <wangcodingrun@gmail.com>
2021-12-20 06:26:53 +0000

    앱에서 스테이트관리하도록 수정

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,49 @@
+import { Component } from "react";
 import "./app.css";
 import Habits from "./components/habits";
 
-function App() {
-  return <Habits />;
+class App extends Component {
+  state = {
+    habits: [
+      { id: 1, name: "Reading", count: 0 },
+      { id: 2, name: "Running", count: 0 },
+      { id: 3, name: "Coding", count: 0 },
+    ],
+  };
+
+  handleIncrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({ habits });
+  };
+
+  handleDescrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const habitCount = habits[index].count - 1;
+
+    habits[index].count = habitCount < 0 ? 0 : habitCount;
+    this.setState({ habits });
+  };
+
+  handleDelete = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits.splice(index, 1);
+    this.setState({ habits });
+  };
+
+  render() {
+    return (
+      <Habits
+        habits={this.state.habits}
+        handleIncrement={this.handleIncrement}
+        handleDescrement={this.handleDescrement}
+        handleDelete={this.handleDelete}
+      />
+    );
+  }
 }
 
 export default App;
--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -2,48 +2,19 @@ import React, { Component } from "react";
 import Habit from "./habit";
 
 class Habits extends Component {
-  state = {
-    habits: [
-      { id: 1, name: "Reading", count: 0 },
-      { id: 2, name: "Running", count: 0 },
-      { id: 3, name: "Coding", count: 0 },
-    ],
-  };
-
-  handleIncrement = (habit) => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    habits[index].count++;
-    this.setState({ habits });
-  };
-
-  handleDescrement = (habit) => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    const habitCount = habits[index].count - 1;
-
-    habits[index].count = habitCount < 0 ? 0 : habitCount;
-    this.setState({ habits });
-  };
-
-  handleDelete = (habit) => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    habits.splice(index, 1);
-    this.setState({ habits });
-  };
-
   render() {
+    const { habits, handleIncrement, handleDescrement, handleDelete } =
+      this.props;
     return (
       <ul>
-        {this.state.habits.map((habit) => {
+        {habits.map((habit) => {
           return (
             <Habit
               key={habit.id}
               habit={habit}
-              onIncrement={this.handleIncrement}
-              onDescrement={this.handleDescrement}
-              onDelete={this.handleDelete}
+              onIncrement={handleIncrement}
+              onDescrement={handleDescrement}
+              onDelete={handleDelete}
             />
           );
         })}
