33319366369ecd077f49a2742dc029fa74b35c9e
Tuyoni <wangcodingrun@gmail.com>
2021-12-21 02:56:33 +0000

    유튜브 리스트  api연결

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import VideoList from "./components/video_list/video_list";
 
 function App() {
-  return <h1>Hello</h1>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&regionCode=US&key=AIzaSyBUZZ8JBtSPdM4gingy3AyukHeM_OqT2uk",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+  return (
+    <div>
+      <h1>Hello</h1> <VideoList videos={videos} />
+    </div>
+  );
 }
 
 export default App;
